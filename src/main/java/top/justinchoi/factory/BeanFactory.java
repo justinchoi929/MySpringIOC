fa64abca7edd170a44bfac3dc398cae16508d8bf
justinchoi <2813970520@qq.com>
2024-10-29 13:08:18 +0800

    实现bean的多线程单例

--- src/main/java/top/justinchoi/factory/BeanFactory.java
+++ src/main/java/top/justinchoi/factory/BeanFactory.java
@@ -2,10 +2,13 @@ package top.justinchoi.factory;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.Properties;
 
 public class BeanFactory {
     private static Properties properties;
+    // 利用缓存思想实现单例
+    private static HashMap<String, Object> cache = new HashMap<>();
     // 获取配置文件中的信息（静态代码块只执行一次）
     static {
         properties = new Properties();
@@ -16,24 +19,36 @@ public class BeanFactory {
         }
     }
 
-    public static Object getDao(){
-        // 得到全类名
-        String value = properties.getProperty("helloDao");
-        // 利用反射机制获取对象
-        try {
-            Class clazz = Class.forName(value);
-            Object object = clazz.getConstructor(null).newInstance(null);
-            return object;
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
+    public static Object getDao(String beanName){
+        // 先判断缓存中是否存在bean
+        if (!cache.containsKey(beanName)) {
+            // 加锁双重检测保证多线程下的线程安全
+            synchronized (BeanFactory.class) {
+                if (!cache.containsKey(beanName)) {
+                    // 不存在则将bean存入缓存
+                    // 利用反射机制获取对象
+                    try {
+                        // 得到全类名
+                        String value = properties.getProperty(beanName);
+                        Class clazz = Class.forName(value);
+                        Object object = clazz.getConstructor(null).newInstance(null);
+                        cache.put(beanName, object);
+                        return object;
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    } catch (InstantiationException e) {
+                        throw new RuntimeException(e);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    } catch (NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
         }
+        return cache.get(beanName);
     }
 }
--- src/main/java/top/justinchoi/service/impl/HelloServiceImpl.java
+++ src/main/java/top/justinchoi/service/impl/HelloServiceImpl.java
@@ -7,7 +7,12 @@ import top.justinchoi.service.HelloService;
 import java.util.List;
 
 public class HelloServiceImpl implements HelloService {
-    private HelloDao helloDao = (HelloDao) BeanFactory.getDao();
+    public HelloServiceImpl() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(BeanFactory.getDao("helloDao"));
+        }
+    }
+    private HelloDao helloDao = (HelloDao) BeanFactory.getDao("helloDao");
     @Override
     public List<String> findAll() {
         return this.helloDao.findAll();
